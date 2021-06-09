c017be44ca9930d0745bfb815cb53e5efe8f8bbc
An Dakyeom <63940054+dakyeoman@users.noreply.github.com>
2021-06-09 22:13:36 +0900

    if else

---
+++ Vanila JS/index.js
@@ -0,0 +1,15 @@
+
+
+const age = prompt("How old are you");
+
+if(age > 18 && age < 24){
+    console.log("CAN DRINK");
+}
+    else if(age > 23){
+    console.log("CAN'T DRINK");
+}
+    else {
+    console.log("too young");
+    }
+console.log("Hi");
+console.log("Hellow");
