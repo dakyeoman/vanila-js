c843fab8e8e3ec642f8ac4ded4332a6e2b525ab9
Dakyeom <63940054+dakyeoman@users.noreply.github.com>
2021-06-25 01:04:03 +0900

    Update app.js

--- Vanila JS/app.js
+++ Vanila JS/app.js
@@ -1,14 +1,18 @@
+
 const h1 = document.querySelector("div.hello:first-child h1");
 
 
 function handleTitleClick() {
-    h1.className = "active";
-    console.log(h1.className);
+    if (h1.className === "active") {
+        h1.className = "";
+    } else {
+        h1.className = "active";
+    } 
+    
 
 }
 
 h1.addEventListener("click", handleTitleClick);
-
 
 
 
