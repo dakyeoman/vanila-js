71904b1def0681db51ecf909da6106fb487cdf0d
An Dakyeom <63940054+dakyeoman@users.noreply.github.com>
2021-11-14 08:47:24 +0900

    Update app.js

--- app.js
+++ app.js
@@ -15,3 +15,41 @@ console.log("hello " + myName)
 const live = null;
 let something; 
 console.log(something, live)
+
+
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+const nonsense = [1, 2, "hello", false, null, true, undefined];
+console.log(daysOfWeek[0]);
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
+
+
+function sayHello(name){
+    console.log("Hello! my name is " + name);
+}
+sayHello("dakyeom");
+
+const calcualator = {
+    add: function(a, b) {
+        return a + b ;
+    },
+    minus: function(a, b)  { 
+        return a - b ;
+    },
+    multiply: function(a, b)  { 
+        return a * b ;
+    },
+    square: function(a, b)  { 
+        return a ** b ;
+    },
+    divide: function(a, b)  {
+        return a / b ;
+    },
+};
+
+const plusResult = calcualator.add(1, 2);
+calcualator.minus(1, 2);
+calcualator.multiply(1, 2);
+calcualator.square(1, 2);
+calcualator.divide(1, 2);
