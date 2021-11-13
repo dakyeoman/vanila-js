74e2aa642dec72d29c0c799765fd5dd44ccb15e8
An Dakyeom <63940054+dakyeoman@users.noreply.github.com>
2021-11-13 10:09:32 +0900

    const and let

---
+++ app.js
@@ -0,0 +1,17 @@
+const a = 5;
+const b = 2;
+let myName = "DK"; 
+
+console.log(a * b);
+console.log(a + b);
+console.log(a / b);
+console.log("hello " + myName)
+
+myName = "Maritza";
+console.log("hello " + myName)
+
+
+
+const live = null;
+let something; 
+console.log(something, live)
