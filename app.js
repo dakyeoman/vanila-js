3acd344ba0848d40f6658b4f30ae137e8a67afbd
An Dakyeom <63940054+dakyeoman@users.noreply.github.com>
2021-11-16 14:35:57 +0900

    update

--- app.js
+++ app.js
@@ -1,10 +1,83 @@
-const title = document.getElementsByTagName("h1");
-console.log(title)
+
+const h1 = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick(){
+    if (h1.className === "active") { 
+        h1.className = "";
+    }else{
+        h1.className = "active";
+    }
+    //is getter & setter. js >>> html >>> css
+}
+
+h1.addEventListener("click", handleTitleClick);
 
 
 
 
 
+
+/*
+const hellos = document.getElementsByClassName("hello");
+console.log(hellos)
+
+const title1 = document.getElementsByTagName("h1");
+console.log(title1)
+
+//! CSS selector //only first one. querySelector; //.class // #id
+const title2 = document.querySelector(".hello h1"); 
+console.log(title2);
+
+const h1 = document.querySelector("div.hello:first-child h1");
+console.log(h1);
+
+console.dir(h1);
+// "on --"" is event but it's  Javascript object.
+
+h1.innerText = "Hello";
+h1.style.color = "blue";
+
+
+//Listen for Events
+function handleTitleClick() {
+    console.log("title was clicked");
+    h1.style.color = "red"
+}
+
+function handleMouseEnter() {
+    console.log("mouse is here");
+    h1.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+    h1.innerText = "Mouse is gone!";
+}
+
+function handleWindowResize() { 
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() { 
+    alert("Copy!")
+}
+
+function handleWindowOffline() {
+    alert("NO WIFI:(")
+}
+
+function handleWindowOnline() { 
+    alert("WIFI ALL GOOOOOD:)")
+}
+
+h1.onclick = handleTitleClick; //it can be 
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize)
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
+*/
 
 
 
@@ -78,6 +151,5 @@ if(isNaN(age)){
 }
 
 */
-
 
 
